1c750f7a0433cf4bca9daac2bcd7f0adca755d70
Denis Brockus <dbrockus@google.com>
2022-11-15 10:09:37 -0700

    libgestures: CY Add more prop_registry coverage
    BUG=b:239447510
    TEST=USE="coverage" FEATURES="test noclean" emerge-brya chromeos-base/gestures
    
    Change-Id: If289dd4099d95f03b98b63aae32c8e1f2cce17e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/gestures/+/4035532
    Reviewed-by: Sean O'Brien <seobrien@chromium.org>
    Commit-Queue: Denis Brockus <dbrockus@chromium.org>
    Reviewed-by: Kenneth Albanowski <kenalba@google.com>
    Auto-Submit: Denis Brockus <dbrockus@chromium.org>
    Tested-by: Denis Brockus <dbrockus@chromium.org>

--- src/prop_registry_unittest.cc
+++ src/prop_registry_unittest.cc
@@ -22,8 +22,13 @@ class PropRegistryTestDelegate : public PropertyDelegate {
  public:
   PropRegistryTestDelegate() : call_cnt_(0) {}
   virtual void BoolWasWritten(BoolProperty* prop) { call_cnt_++; };
+  virtual void BoolArrayWasWritten(BoolArrayProperty* prop) { call_cnt_++; };
   virtual void DoubleWasWritten(DoubleProperty* prop) { call_cnt_++; };
+  virtual void DoubleArrayWasWritten(DoubleArrayProperty* prop) {
+    call_cnt_++;
+  };
   virtual void IntWasWritten(IntProperty* prop) { call_cnt_++; };
+  virtual void IntArrayWasWritten(IntArrayProperty* prop) { call_cnt_++; };
   virtual void StringWasWritten(StringProperty* prop) { call_cnt_++; };
 
   int call_cnt_;
@@ -82,6 +87,41 @@ TEST(PropRegistryTest, SimpleTest) {
   EXPECT_TRUE(strstr(ValueForProperty(stp2).c_str(), "bar"));
   stp2.HandleGesturesPropWritten();
   EXPECT_EQ(expected_call_cnt, delegate.call_cnt_);
+
+
+  Json::Value my_bool_val = bp1.NewValue();
+  Json::Value my_int_val = ip1.NewValue();
+  Json::Value my_double_val = dp1.NewValue();
+  Json::Value my_str_val = stp1.NewValue();
+  EXPECT_TRUE(bp1.SetValue(my_bool_val));
+  EXPECT_FALSE(bp1.SetValue(my_int_val));
+  EXPECT_FALSE(bp1.SetValue(my_double_val));
+  EXPECT_FALSE(bp1.SetValue(my_str_val));
+
+  EXPECT_FALSE(ip1.SetValue(my_bool_val));
+  EXPECT_TRUE(ip1.SetValue(my_int_val));
+  EXPECT_FALSE(ip1.SetValue(my_double_val));
+  EXPECT_FALSE(ip1.SetValue(my_str_val));
+
+  EXPECT_FALSE(dp1.SetValue(my_bool_val));
+  EXPECT_TRUE(dp1.SetValue(my_int_val));
+  EXPECT_TRUE(dp1.SetValue(my_double_val));
+  EXPECT_FALSE(dp1.SetValue(my_str_val));
+
+  EXPECT_FALSE(stp1.SetValue(my_bool_val));
+  EXPECT_FALSE(stp1.SetValue(my_int_val));
+  EXPECT_FALSE(stp1.SetValue(my_double_val));
+  EXPECT_TRUE(stp1.SetValue(my_str_val));
+
+  // This code does not do anything but the code coverage
+  // hits for not covered areas due to not running the
+  // unused default virtual functions.
+  PropertyDelegate pd;
+
+  pd.BoolWasWritten(&bp1);
+  pd.DoubleWasWritten(&dp1);
+  pd.IntWasWritten(&ip1);
+  pd.StringWasWritten(&stp1);
 }
 
 TEST(PropRegistryTest, PropChangeTest) {
@@ -184,4 +224,101 @@ TEST(PropRegistryTest, DoublePromoteIntTest) {
   EXPECT_TRUE(my_double.SetValue(my_int_val));
   EXPECT_TRUE(strstr(ValueForProperty(my_double).c_str(), "321"));
 }
+
+TEST(PropRegistryTest, BoolArrayTest) {
+  PropRegistry reg;
+  PropRegistry reg_with_delegate;
+  PropRegistryTestDelegate delegate;
+
+  GesturesPropBool vals[] = { false, true };
+  BoolArrayProperty my_bool_array_w_delegate(
+    &reg, "MyBoolArray", vals, 2, &delegate);
+  EXPECT_EQ(0, delegate.call_cnt_);
+  my_bool_array_w_delegate.HandleGesturesPropWritten();
+  EXPECT_EQ(1, delegate.call_cnt_);
+  delegate.BoolArrayWasWritten(&my_bool_array_w_delegate);
+  EXPECT_EQ(2, delegate.call_cnt_);
+
+  IntProperty ip1(&reg, "hi", 567, &delegate);
+  StringProperty stp1(&reg, "hi", "foo", &delegate);
+  Json::Value my_bool_array_val = my_bool_array_w_delegate.NewValue();
+  Json::Value my_int_val = ip1.NewValue();
+  Json::Value my_str_val = stp1.NewValue();
+  EXPECT_FALSE(my_bool_array_w_delegate.SetValue(my_int_val));
+  EXPECT_FALSE(my_bool_array_w_delegate.SetValue(my_str_val));
+  EXPECT_TRUE(my_bool_array_w_delegate.SetValue(my_bool_array_val));
+
+  // This code does not do anything but the code coverage
+  // hits for not covered areas due to not running the
+  // unused default virtual functions.
+  PropertyDelegate pd;
+
+  BoolArrayProperty my_bool_array(&reg, "MyBoolArray", vals, 2);
+  pd.BoolArrayWasWritten(&my_bool_array);
+}
+
+TEST(PropRegistryTest, DoubleArrayTest) {
+  PropRegistry reg;
+  PropRegistry reg_with_delegate;
+  PropRegistryTestDelegate delegate;
+
+  double vals[] = { 0.0, 1.0 };
+  DoubleArrayProperty my_double_array_w_delegate(
+    &reg, "MyDoubleArray", vals, 2, &delegate);
+  EXPECT_EQ(0, delegate.call_cnt_);
+  my_double_array_w_delegate.HandleGesturesPropWritten();
+  EXPECT_EQ(1, delegate.call_cnt_);
+  delegate.DoubleArrayWasWritten(&my_double_array_w_delegate);
+  EXPECT_EQ(2, delegate.call_cnt_);
+
+  IntProperty ip1(&reg, "hi", 567, &delegate);
+  StringProperty stp1(&reg, "hi", "foo", &delegate);
+  Json::Value my_double_array_val = my_double_array_w_delegate.NewValue();
+  Json::Value my_int_val = ip1.NewValue();
+  Json::Value my_str_val = stp1.NewValue();
+  EXPECT_FALSE(my_double_array_w_delegate.SetValue(my_int_val));
+  EXPECT_FALSE(my_double_array_w_delegate.SetValue(my_str_val));
+  EXPECT_TRUE(my_double_array_w_delegate.SetValue(my_double_array_val));
+
+  // This code does not do anything but the code coverage
+  // hits for not covered areas due to not running the
+  // unused default virtual functions.
+  PropertyDelegate pd;
+
+  DoubleArrayProperty my_double_array(&reg, "MyDoubleArray", vals, 2);
+  pd.DoubleArrayWasWritten(&my_double_array);
+}
+
+TEST(PropRegistryTest, IntArrayTest) {
+  PropRegistry reg;
+  PropRegistry reg_with_delegate;
+  PropRegistryTestDelegate delegate;
+
+  int vals[] = { 0, 1 };
+  IntArrayProperty my_int_array_w_delegate(
+    &reg, "MyIntArray", vals, 2, &delegate);
+  EXPECT_EQ(0, delegate.call_cnt_);
+  my_int_array_w_delegate.HandleGesturesPropWritten();
+  EXPECT_EQ(1, delegate.call_cnt_);
+  delegate.IntArrayWasWritten(&my_int_array_w_delegate);
+  EXPECT_EQ(2, delegate.call_cnt_);
+
+  IntProperty ip1(&reg, "hi", 567, &delegate);
+  StringProperty stp1(&reg, "hi", "foo", &delegate);
+  Json::Value my_int_array_val = my_int_array_w_delegate.NewValue();
+  Json::Value my_int_val = ip1.NewValue();
+  Json::Value my_str_val = stp1.NewValue();
+  EXPECT_FALSE(my_int_array_w_delegate.SetValue(my_int_val));
+  EXPECT_FALSE(my_int_array_w_delegate.SetValue(my_str_val));
+  EXPECT_TRUE(my_int_array_w_delegate.SetValue(my_int_array_val));
+
+  // This code does not do anything but the code coverage
+  // hits for not covered areas due to not running the
+  // unused default virtual functions.
+  PropertyDelegate pd;
+
+  IntArrayProperty my_int_array(&reg, "MyIntArray", vals, 2);
+  pd.IntArrayWasWritten(&my_int_array);
+}
+
 }  // namespace gestures
